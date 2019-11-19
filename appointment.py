c791a94d2cf298a34b199f1d0a62473bc4f58f4d
OrdinaryJohn <57931632+OrdinaryJohn@users.noreply.github.com>
2019-11-18 22:29:02 -0600

    Add files via upload

---
+++ appointment.py
@@ -0,0 +1,118 @@
+# import modules
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+# connect to the databse.
+conn = sqlite3.connect('database.db')
+# cursor to move around the databse
+c = conn.cursor()
+# empty list to later append the ids from the database
+ids = []
+# tkinter window
+class Application:
+    def __init__(self, master):
+        self.master = master
+
+        # creating the frames in the master
+        self.left = Frame(master, width=800, height=720, bg='purple')
+        self.left.pack(side=LEFT)
+
+        self.right = Frame(master, width=400, height=720, bg='steelblue')
+        self.right.pack(side=RIGHT)
+
+        #Label for window       
+        self.heading = Label(self.left, text="Mankato State Tutor Center", font=('arial 40 bold'), fg='black', bg='gold')
+        self.heading.place(x=0, y=0)
+
+
+        #Student Id
+        self.studentid = Label(self.left, text="Student's ID", font=('arial 18 bold'), fg='black', bg='gold')
+        self.studentid.place(x=0, y=100)
+    
+        #Student's name
+        self.name = Label(self.left, text="Student's Name", font=('arial 18 bold'), fg='black', bg='gold')
+        self.name.place(x=0, y=140)
+
+        #Major
+        self.major = Label(self.left, text="Major", font=('arial 18 bold'), fg='black', bg='gold')
+        self.major.place(x=0, y=180)
+
+        #Appointment time
+        self.scheduled_time = Label(self.left, text="Appointment Time", font=('arial 18 bold'), fg='black', bg='gold')
+        self.scheduled_time.place(x=0, y=220)
+
+        #Phone Number
+        self.phone = Label(self.left, text="Phone Number", font=('arial 18 bold'), fg='black', bg='gold')
+        self.phone.place(x=0, y=260)
+
+        # Entries for all labels-----------------------------------------------
+        self.studentid_ent = Entry(self.left, width=30)
+        self.studentid_ent.place(x=250, y=105)
+
+        self.name_ent = Entry(self.left, width=30)
+        self.name_ent.place(x=250, y=145)
+
+        self.major_ent = Entry(self.left, width=30)
+        self.major_ent.place(x=250, y=185)
+
+        self.scheduled_time_ent = Entry(self.left, width=30)
+        self.scheduled_time_ent.place(x=250, y=230)
+
+        self.phone_ent = Entry(self.left, width=30)
+        self.phone_ent.place(x=250, y=275)
+
+        #Button to perform a command
+        self.submit = Button(self.left, text="Add Appointment", width=20, height=2, bg='steelblue', command=self.add_appointment)
+        self.submit.place(x=300, y=340)
+    
+        #Getting the number of appointments fixed to view in the log
+        sql2 = "SELECT ID FROM appointments "
+        self.result = c.execute(sql2)
+        for self.row in self.result:
+            self.id = self.row[0]
+            ids.append(self.id)
+        
+        #Ordering the ids
+        self.new = sorted(ids)
+        self.final_id = self.new[len(ids)-1]
+        #Displaying the logs in our right frame
+        self.logs = Label(self.right, text="Logs", font=('arial 28 bold'), fg='white', bg='steelblue')
+        self.logs.place(x=1, y=1)
+
+        self.box = Text(self.right, width=50, height=40)
+        self.box.place(x=20, y=60)
+        self.box.insert(END, "Number of appointments today:  " + str(self.final_id))
+    #Funtion to call when the submit button is clicked
+    def add_appointment(self):
+        #Getting the user inputs
+        self.var1 = self.studentid_ent.get()
+        self.var2 = self.name_ent.get()
+        self.var3 = self.major_ent.get()
+        self.var4 = self.scheduled_time_ent.get()
+        self.var5 = self.phone_ent.get()
+
+        #Checking if the user input is empty
+        if self.var1 == '' or self.var2 == '' or self.var3 == '' or self.var4 == '' or self.var5 == '':
+            messagebox.showinfo("Warning", "Please Fill Up All Boxes")
+        else:
+            #Adding to database
+            sql = "INSERT INTO 'appointments' (studentid, name, major, scheduled_time, phone) VALUES(?, ?, ?, ?, ?)"
+            c.execute(sql, (self.var1, self.var2, self.var3, self.var4, self.var5))
+            conn.commit()
+            messagebox.showinfo("Success", "Appointment for " +str(self.var2) + " has been created" )
+            
+
+            self.box.insert(END, '\nAppointment fixed for ' + str(self.var2) + ' at ' + str(self.var4))
+
+# creating the object
+root = Tk()
+b = Application(root)
+
+# resolution of the window
+root.geometry("1200x720+0+0")
+
+# preventing the resize feature
+root.resizable(False, False)
+
+# end the loop
+root.mainloop()
---
+++ update.py
@@ -0,0 +1,108 @@
+# update the appointments
+from tkinter import *
+from tkinter import messagebox 
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+
+class Application:
+    def __init__(self, master):
+        master.title("Update")
+        master.configure(background='purple')
+        self.master = master
+        # heading label
+        self.heading = Label(master, text="Update Appointments",  fg='black', bg='gold', font=('arial 40 bold'))
+        self.heading.place(x=0, y=0)
+
+        # search criteria -->student id 
+        self.studentid = Label(master, text="Enter Student ID", bg='gold', font=('arial 18 bold'))
+        self.studentid.place(x=0, y=80)
+
+        # entry for  the student id
+        self.studentidnet = Entry(master, width=30)
+        self.studentidnet.place(x=280, y=90)
+
+        # search button
+        self.search = Button(master, text="Search", width=12, height=1, bg='steelblue', command=self.search_db)
+        self.search.place(x=350, y=120)
+    # function to search
+    def search_db(self):
+        self.input = self.studentidnet.get()
+        # execute sql 
+
+        sql = "SELECT * FROM appointments WHERE studentid LIKE ?"
+        self.res = c.execute(sql, (self.input,))
+        for self.row in self.res:
+            self.studentid = self.row[0]
+            self.name = self.row[1]
+            self.major = self.row[2]
+            self.time = self.row[3]
+            self.phone = self.row[4]
+
+        # creating the update form
+        self.uname = Label(self.master, text="Student's Name", bg='gold', font=('arial 18 bold'))
+        self.uname.place(x=0, y=160)
+
+        self.umajor = Label(self.master, text="Major", bg='gold', font=('arial 18 bold'))
+        self.umajor.place(x=0, y=200)
+
+        self.uscheduled_time = Label(self.master, text="Appointment Time", bg='gold', font=('arial 18 bold'))
+        self.uscheduled_time.place(x=0, y=240)
+
+        self.uphone = Label(self.master, text="Phone Number", bg='gold', font=('arial 18 bold'))
+        self.uphone.place(x=0, y=280)
+
+        # entries for each labels==========================================================
+        # ===================filling the search result in the entry box to update
+        self.ent1 = Entry(self.master, width=30)
+        self.ent1.place(x=300, y=170)
+        self.ent1.insert(END, str(self.name))
+
+        self.ent2 = Entry(self.master, width=30)
+        self.ent2.place(x=300, y=210)
+        self.ent2.insert(END, str(self.major))
+
+        self.ent3 = Entry(self.master, width=30)
+        self.ent3.place(x=300, y=250)
+        self.ent3.insert(END, str(self.time))
+
+        self.ent4 = Entry(self.master, width=30)
+        self.ent4.place(x=300, y=290)
+        self.ent4.insert(END, str(self.phone))
+
+        # button to execute update
+        self.update = Button(self.master, text="Update", width=20, height=2, bg='lightblue', command=self.update_db)
+        self.update.place(x=400, y=380)
+
+        # button to delete
+        self.delete = Button(self.master, text="Delete", width=20, height=2, bg='red', command=self.delete_db)
+        self.delete.place(x=150, y=380)
+    def update_db(self):
+        # declaring the variables to update
+        self.var1 = self.ent1.get() #updated name
+        self.var2 = self.ent2.get() #updated major
+        self.var3 = self.ent3.get() #updated scheduled_time
+        self.var4 = self.ent4.get() #updated phone
+
+        query = "UPDATE appointments SET name=?, major=?, scheduled_time=?, phone=? WHERE studentid LIKE ?"
+        c.execute(query, (self.var1, self.var2, self.var3, self.var4, self.studentidnet.get(),))
+        conn.commit()
+        messagebox.showinfo("Updated", "Successfully Updated.")
+    def delete_db(self):
+        # delete the appointment
+        sql2 = "DELETE FROM appointments WHERE studentid LIKE ?"
+        c.execute(sql2, (self.studentidnet.get(),))
+        conn.commit()
+        messagebox.showinfo("Success", "Deleted Successfully")
+        self.ent1.destroy()
+        self.ent2.destroy()
+        self.ent3.destroy()
+        self.ent4.destroy()
+
+# creating the object
+root = Tk()
+b = Application(root)
+root.geometry("1200x720+0+0")
+root.resizable(True, True)
+root.mainloop()
